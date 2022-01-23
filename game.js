34c003137ff76c3ed199544e5347f9281afdea51
Szymon Popiolek <2004szymonp@gmail.com>
2022-01-23 19:12:20 +0100

    Added more sounds
    Added specific for each color

--- game.js
+++ game.js
@@ -1,16 +1,24 @@
 
 arrayColors = ["blue", "red", "yellow", "green"];
 colorArray = [];
+clickedPattern = [];
+
+$(".btn").click(function() {
+	let userChosenColor = $(this).attr("id");
+	clickedPattern.push(userChosenColor);
+
+	playSound(userChosenColor);
+});
 
 function nextSequence() {
 	let randomColor = Math.floor(Math.random() * 4);
-
 	let chosenColor = arrayColors[randomColor];
 
 	colorArray.push(chosenColor);
-
 	$("#" + chosenColor).fadeIn(100).fadeOut(100).fadeOut(100);
+}
 
-	let sound = new Audio("sound/" + chosenColor + "mp3");
-	Audio.play();
+function playSound(name) {
+	let chosenSound = new Audio("sounds/" + name + ".mp3");
+	chosenSound.play(chosenSound);
 }
